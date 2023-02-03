da34e4ee18746efe8e624426064a23949640041b
abbygarcia1517 <89549227+abbygarcia1517@users.noreply.github.com>
2023-02-03 15:28:11 -0600

    Commit

---
+++ app.js
@@ -0,0 +1,20 @@
+function logger() {
+    const name = "Timesheet";
+    console.log("Clock In");
+    console.log("Clock Out");
+    console.log("Clock In");
+    console.log("Clock Out");
+
+}
+
+logger(name);
+
+
+const title = "Leave Balance";
+
+function balance(num1, num2) {
+    console.log(num1 + num2);
+    console.log(num1 - num2);
+}
+
+balance(20, 5);
